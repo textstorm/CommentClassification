8be76c7677f59b2a19b2cdfaf1690f73c8f15dc0
textstorm <jiangzhewise@gmail.com>
2017-12-29 13:17:33 +0800

    debug

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import os
 import utils
 import time
 import config
-import pandas as pd
 import tensorflow as tf
-from tensorflow.python.ops import lookup_ops
-from model import TextCNN, TextRNN
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+def run_eval(args, dev_model, dev_sess, model_dir):
+  with dev_model.graph.as_default():
+    loaded_dev_model, global_step = helper.create_or_load_model(
+        dev_model.model, model_dir, dev_sess, "dev")
+
+  _run_eval(args, loaded_infer_model, global_step, infer_sess, infer_model.iterator)
+
+def _sample_generate(args, model, global_step, sess, iterator):
+  if iterator is not None:
+    sess.run(iterator.initializer)
+
+  sample_id, sample_words = model.infer(sess)
+  if args.beam_width > 0:
+    sample_words = sample_words[0]
+
+  for sample in sample_words: # many sent
+    utils.print_out("gen sentence: %s" % (" ".join(sample)))
 
 def main(args):
 
@@ -21,65 +36,60 @@ def main(args):
   if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-  with tf.Graph().as_default():
-    vocab_table = lookup_ops.index_table_from_file(vocab_dir, default_value=0)
-    dataset = tf.data.TextLineDataset(data_dir)
+  train_model = helper.build_train_model(args)
+  dev_model = helper.build_eval_model(args)
 
-    iterator = utils.get_iterator(
-        dataset=dataset,
-        vocab_table=vocab_table,
-        batch_size=args.batch_size,
-        max_len=args.sentence_length,
-        random_seed=args.random_seed,
-        shuffle=True)
+  config_proto = utils.get_config_proto()
+  train_sess = tf.Session(config=config_proto, graph=train_model.graph)
+  dev_sess = tf.Session(config=config_proto, graph=dev_model.graph)
 
-    config_proto = utils.get_config_proto()
-    sess = tf.Session(config=config_proto)
-    if args.model_type == "cnn":
-      model = TextCNN(args, iterator, "TextCNN")
-      # test_batch = utils.get_batches(test_x, test_y, args.max_size)
-    elif args.model_type in ["rnn", "bi_rnn"]:
-      model = TextRNN(args, iterator, "TextRNN")
-      # test_batch = utils.get_batches(test_x, test_y, args.max_size, type="rnn")
+  with train_model.graph.as_default():
+    loaded_train_model, global_step = helper.create_or_load_model(
+        train_model.model, args.save_dir, train_sess, name="train")
 
-    sess.run(tf.global_variables_initializer())
+    sess.run(model.iterator.initializer)
     summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
 
-    for epoch in range(1, args.nb_epochs + 1):
-      print "epoch %d start" % epoch
-      print "- " * 50
+    loss = 0.
+    total_reviews = 0
+    epoch = 1
+    epoch_start_time = time.time()
+    step_start_time = epoch_start_time
 
-      loss = 0.
-      total_reviews = 0
-      accuracy = 0.
+    print "Epoch %d start " % (epoch)
+    print "- " * 50
 
-      epoch_start_time = time.time()
-      step_start_time = epoch_start_time
-      for idx, batch in enumerate(train_batch):
-        _, loss_t, accuracy_t, global_step, batch_size, summaries = model.train(sess, args.keep_prob)
+    for step in range(args.max_step):
+      try:
+        _, loss_t, global_step, batch_size, summaries = model.train(sess, args.keep_prob)
 
         loss += loss_t * batch_size
         total_reviews += batch_size
-        accuracy += accuracy_t * batch_size
         summary_writer.add_summary(summaries, global_step)
 
-        if global_step % 50 == 0:
-          print "epoch %d, step %d, loss %f, accuracy %.4f, time %.2fs" % \
-            (epoch, global_step, loss_t, accuracy_t, time.time() - step_start_time)
+        if global_step % 100 == 0:
+          print "epoch %d, step %d, loss %f, time %.2fs" % \
+            (epoch, global_step, loss_t, time.time() - step_start_time)
           step_start_time = time.time()
 
-      epoch_time = time.time() - epoch_start_time
-      print "%.2f seconds in this epoch" % (epoch_time)
-      print "train loss %f, train accuracy %.4f" % (loss / total_reviews, accuracy / total_reviews)
+      except tf.errors.OutOfRangeError:
+        print "epoch %d finish, time %.2fs" % (epoch, time.time() - epoch_start_time)
+        print "- " * 50
+        sess.run(model.iterator.initializer)
+        epoch_time = time.time() - epoch_start_time
+        epoch_start_time = time.time()
+        print "%.2f seconds in this epoch" % (epoch_time)
+        print "train loss %f total comments %d" % (loss / total_reviews, total_reviews)
+        loaded_train_model.saver.save(train_sess,
+                      os.path.join(args.save_dir, "model.ckpt"), global_step=global_step)
+        run_eval(args, dev_model, dev_sess, args.save_dir)
 
-      # total_reviews = 0
-      # accuracy = 0.
-      # for batch in test_batch:
-      #   reviews, reviews_length, labels = batch
-      #   accuracy_t, batch_size = model.test(sess, reviews, reviews_length, labels, 1.0)
-      #   total_reviews += batch_size
-      #   accuracy += accuracy_t * batch_size
-      # print "accuracy %.4f in %d test reviews" % (accuracy / total_reviews, total_reviews)
+        total_reviews = 0
+        loss = 0.
+        epoch += 1
+        print "Epoch %d start " % (epoch)
+        print "- " * 50
+        continue
 
 if __name__ == '__main__':
   args = config.get_args()
