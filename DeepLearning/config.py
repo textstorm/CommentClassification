04a6da17b0b2c0309e6a27d89ba93049f48099fe
textstorm <jiangzhewise@gmail.com>
2018-01-29 20:05:40 +0800

    update

---
+++ DeepLearning/config.py
@@ -0,0 +1,46 @@
+
+import argparse
+
+def get_args():
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--random_seed', type=int, default=1023, help='random seed')
+  #data
+  parser.add_argument('--train_dir', type=str, default='data/pre/train.csv', help="train data")
+  parser.add_argument('--valid_dir', type=str, default='data/pre/dev.csv', help="valid data")
+  parser.add_argument('--test_dir', type=str, default='data/pre/test.csv', help="test data")
+  parser.add_argument('--sub_dir', type=str, default='data/pre/submission.csv', help="submission save")
+  parser.add_argument('--vocab_dir', type=str, default='data/pre/vocab.txt', help='vocab dir')
+  parser.add_argument('--wordvec_dir', type=str, default='data/glove/crawl-300d-2M.vec', help='fasttext dir')
+
+  #save and logs
+  parser.add_argument('--log_dir', type=str, default='save/logs')
+  parser.add_argument('--cnn_save_dir', type=str, default='save/saves/cnn')
+  parser.add_argument('--rnn_save_dir', type=str, default='save/saves/rnn')
+
+  #model
+  parser.add_argument('--model_type', type=str, default="rnn", help='cnn,gru,lstm')
+  parser.add_argument('--nb_classes', type=int, default=6, help='class numbers')
+  parser.add_argument('--max_len', type=int, default=500, help='The length of input x')
+  parser.add_argument('--vocab_size', type=int, default=30001, help='data vocab size')
+  parser.add_argument('--embed_size', type=int, default=300, help='dims of word embedding')
+
+  #cnn
+  parser.add_argument('--filter_sizes', type=list, default=[3, 4, 5], help='')
+  parser.add_argument('--num_filters', type=int, default=100, help='num of filters')
+  parser.add_argument('--keep_prob_cnn', type=float, default=0.5, help='keep prob in cnn')
+  parser.add_argument('--max_size_cnn', type=int, default=1000, help='max numbers every batch of cnn')
+  parser.add_argument('--max_step_cnn', type=int, default=12000, help='max cnn train step')   
+  #rnn
+  parser.add_argument('--cell_type', type=str, default="gru", help='lstm or gru')
+  parser.add_argument('--hidden_size', type=int, default=64, help='rnn hidden size')
+  parser.add_argument('--rnn_layers', type=int, default=2, help='rnn layers')
+  parser.add_argument('--keep_prob_rnn', type=float, default=0.5, help='keep prob in rnn')
+  parser.add_argument('--max_size_rnn', type=int, default=100, help='max numbers every batch of rnn')
+  parser.add_argument('--max_step_rnn', type=int, default=8000, help='max rnn train step')
+
+  parser.add_argument('--batch_size', type=int, default=32, help='Example numbers every batch')
+  parser.add_argument('--learning_rate', type=float, default=0.0005, help='initial learning rate')
+  parser.add_argument('--max_grad_norm', type=float, default=10.0, help='max norm of gradient')
+
+  return parser.parse_args()
---
+++ DeepLearning/main.py
@@ -0,0 +1,153 @@
+
+import os
+import utils
+import time
+import config
+import helper
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+def run_valid(args, valid_model, valid_sess, model_dir):
+  with valid_model.graph.as_default():
+    loaded_valid_model, global_step = helper.create_or_load_model(
+        valid_model.model, model_dir, valid_sess, "dev")
+
+  return _run_valid(loaded_valid_model, global_step, valid_sess, valid_model.iterator)
+
+def _run_valid(model, global_step, sess, iterator):
+  sess.run(iterator.initializer)
+  total_loss, data_size = 0., 0
+  while True:
+    try:
+      loss_t, batch_size = model.test(sess, 1.)
+      total_loss += loss_t * batch_size
+      data_size += batch_size
+    except tf.errors.OutOfRangeError:
+      break
+
+  avg_loss = total_loss / data_size
+  return avg_loss
+
+def main(args):
+  #dir
+  pretrain_dir = args.wordvec_dir
+  if args.model_type == "cnn": 
+    save_dir = args.cnn_save_dir
+    max_step = args.max_step_cnn
+    keep_prob = args.keep_prob_cnn
+  elif args.model_type == "rnn": 
+    save_dir = args.rnn_save_dir
+    max_step = args.max_step_rnn
+    keep_prob = args.keep_prob_rnn
+
+  if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+  train_model = helper.build_train_model(args)
+  valid_model = helper.build_eval_model(args)
+
+  config_proto = utils.get_config_proto()
+  train_sess = tf.Session(config=config_proto, graph=train_model.graph)
+  valid_sess = tf.Session(config=config_proto, graph=valid_model.graph)
+
+  with train_model.graph.as_default():
+    loaded_train_model, global_step = helper.create_or_load_model(
+        train_model.model, save_dir, train_sess, name="train")
+
+  train_sess.run(train_model.iterator.initializer)
+  epoch = 1
+  loss, total_reviews = 0., 0
+  epoch_start_time = time.time()
+  step_start_time = epoch_start_time
+
+  print "Word vector use %s" % (args.wordvec_dir.split("/")[-1])
+  print "Model type is %s" % (args.model_type)
+  print "Epoch %d start " % (epoch)
+  print "- " * 50
+  vocab = utils.load_data(args.vocab_dir)
+  embedding = utils.load_fasttext(pretrain_dir, vocab)
+  train_sess.run(loaded_train_model.embedding_init, {loaded_train_model.embedding_placeholder: embedding})
+  for line in loaded_train_model.tvars:
+    print line
+
+  for step in range(max_step):
+    try:
+      _, loss_t, global_step, batch_size = loaded_train_model.train(train_sess, keep_prob)
+
+      loss += loss_t * batch_size
+      total_reviews += batch_size
+
+      if global_step % 100 == 0:
+        print "epoch %d, step %d, loss %f, time %.2fs" % \
+          (epoch, global_step, loss_t, time.time() - step_start_time)   
+        step_start_time = time.time()
+
+      if global_step % 1000 == 0:
+        loaded_train_model.saver.save(train_sess,
+            os.path.join(save_dir, "model.ckpt"), global_step=global_step)   
+        avg_loss = run_valid(args, valid_model, valid_sess, save_dir)
+        print "valid loss %f after train step %d" % (avg_loss, global_step)
+        step_start_time = time.time()        
+
+    except tf.errors.OutOfRangeError:
+      print "epoch %d finish, time %.2fs" % (epoch, time.time() - epoch_start_time)
+      print "- " * 50
+      epoch_time = time.time() - epoch_start_time
+      print "%.2f seconds in this epoch" % (epoch_time)
+      print "train loss %f in this epoch" % (loss / total_reviews)
+      train_sess.run(train_model.iterator.initializer)
+      epoch_start_time = time.time()
+      loss, total_reviews = 0., 0
+      epoch += 1
+      print "Epoch %d start " % (epoch)
+      print "- " * 50
+      step_start_time = time.time()
+      continue
+
+def run_test(args, test_model, test_sess, model_dir):
+  with test_model.graph.as_default():
+    loaded_test_model, global_step = helper.create_or_load_model(
+        test_model.model, model_dir, test_sess, "test")
+
+  _run_test(args, loaded_test_model, global_step, test_sess, test_model.iterator)
+
+def _run_test(args, model, global_step, sess, iterator):
+  sess.run(iterator.initializer)
+  total_logits = []
+  while True:
+    try:
+      logits = model.get_logits(sess, 1.).tolist()
+      total_logits += logits
+    except tf.errors.OutOfRangeError:
+      break
+  print np.array(total_logits).shape
+  write_results(np.asarray(total_logits, dtype=np.float64))
+
+def write_results(logits):
+  data = pd.read_csv(args.test_dir)
+  columns = ['id', 'toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+  label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+  data = data.reindex(columns=columns)
+  data[label_cols] = logits
+  data.to_csv(args.sub_dir, index=False)
+
+def test(args):
+  if args.model_type == "cnn": 
+    save_dir = args.cnn_save_dir
+  elif args.model_type == "rnn": 
+    save_dir = args.rnn_save_dir
+
+  test_model = helper.build_test_model(args)
+  config_proto = utils.get_config_proto()
+  test_sess = tf.Session(config=config_proto, graph=test_model.graph)
+
+  start_time = time.time()
+  run_test(args, test_model, test_sess, save_dir)
+
+if __name__ == '__main__':
+  args = config.get_args()
+  main(args)
+  test(args)
