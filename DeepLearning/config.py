cbce8feddcf12889736ed313ac7da125faa1709b
textstorm <jiangzhewise@gmail.com>
2018-02-04 15:52:58 +0800

    update

--- DeepLearning/config.py
+++ DeepLearning/config.py
@@ -18,10 +18,10 @@ def get_args():
   parser.add_argument('--save_dir', type=str, default='save/saves')
 
   #model
-  parser.add_argument('--model_type', type=str, default="attention", help='cnn,gru,lstm,attention')
+  parser.add_argument('--model_type', type=str, default="rnn", help='cnn,rnn,attention')
   parser.add_argument('--nb_classes', type=int, default=6, help='class numbers')
   parser.add_argument('--max_len', type=int, default=500, help='The length of input x')
-  parser.add_argument('--vocab_size', type=int, default=30001, help='data vocab size')
+  parser.add_argument('--vocab_size', type=int, default=20001, help='data vocab size')
   parser.add_argument('--embed_size', type=int, default=300, help='dims of word embedding')
 
   #cnn
@@ -31,13 +31,14 @@ def get_args():
   parser.add_argument('--max_size_cnn', type=int, default=1000, help='max numbers every batch of cnn')
   parser.add_argument('--max_step_cnn', type=int, default=12000, help='max cnn train step')   
   #rnn
-  parser.add_argument('--cell_type', type=str, default="rnn", help='lstm or gru')
+  parser.add_argument('--cell_type', type=str, default="gru", help='lstm or gru')
   parser.add_argument('--hidden_size', type=int, default=64, help='rnn hidden size')
   parser.add_argument('--attention_size', type=int, default=32, help='attention size')
   parser.add_argument('--rnn_layers', type=int, default=2, help='rnn layers')
   parser.add_argument('--keep_prob_rnn', type=float, default=0.5, help='keep prob in rnn')
   parser.add_argument('--max_size_rnn', type=int, default=100, help='max numbers every batch of rnn')
-  parser.add_argument('--max_step_rnn', type=int, default=8000, help='max rnn train step')
+  parser.add_argument('--max_step_rnn', type=int, default=8100, help='max rnn train step')
+  parser.add_argument('--ispool', type=bool, default=False, help='is pooling or last time')
 
   parser.add_argument('--batch_size', type=int, default=32, help='Example numbers every batch')
   parser.add_argument('--learning_rate', type=float, default=0.0005, help='initial learning rate')
