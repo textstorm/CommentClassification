ed0fb66f922c1b9e33a12ba7234ca749de52b6c8
textstorm <jiangzhewise@gmail.com>
2018-03-04 09:02:27 +0800

    update

--- NEW/main.py
+++ NEW/main.py
@@ -11,7 +11,7 @@ from sklearn.metrics import roc_auc_score
 from model import TextCNN, TextRNN, TextFNN
 import tensorflow as tf
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 def main(args):
   print "loadding data and labels from dataset"
@@ -27,7 +27,7 @@ def main(args):
   y = train[target_cols].values
 
   index2word, word2index = utils.load_vocab(args.vocab_dir)
-  x_vector = utils.vectorize(x, word2index, verbose=True)
+  x_vector = utils.vectorize(x, word2index, verbose=False)
   x_vector = np.array(x_vector)
 
   pretrain_dir = args.wordvec_dir
@@ -43,9 +43,8 @@ def main(args):
     os.makedirs(save_dir)
 
   nfolds = args.nfolds
-  # skf = StratifiedKFold(n_splits=nfolds, random_state=123)
   skf = KFold(n_splits=nfolds, shuffle=True, random_state=123)
-
+  test_prob = []
   for (f, (train_index, test_index)) in enumerate(skf.split(x_vector, y)):
     x_train, x_eval = x_vector[train_index], x_vector[test_index]
     y_train, y_eval = y[train_index], y[test_index]
@@ -69,16 +68,16 @@ def main(args):
       for line in model.tvars:
         print line
 
-      print "training %s model for %s" % (args.model_type, col)
+      print "training %s model for toxic comments classification" % (args.model_type)
       for epoch in range(1, args.nb_epochs + 1):
         print "epoch %d start" % epoch, "\n", "- " * 50
         loss, total_comments = 0., 0
         if args.model_type in ["cnn", "fnn"]:
           train_batch = utils.get_batches(x_train, y_train, args.batch_size, args.max_len)
-          test_batch = utils.get_batches(x_eval, y_eval, args.max_size_cnn, args.max_len)
+          valid_batch = utils.get_batches(x_eval, y_eval, args.max_size_cnn, args.max_len)
         elif args.model_type in ["rnn"]:
           train_batch = utils.get_batches(x_train, y_train, args.batch_size, args.max_len, type="rnn")
-          test_batch = utils.get_batches(x_eval, y_eval, args.max_size_rnn, type="rnn")
+          valid_batch = utils.get_batches(x_eval, y_eval, args.max_size_rnn, type="rnn")
 
         epoch_start_time = time.time()
         step_start_time = epoch_start_time
@@ -93,34 +92,68 @@ def main(args):
           if global_step % 100 == 0:
             print "epoch %d, step %d, loss %f, time %.2fs" % \
               (epoch, global_step, loss_t, time.time() - step_start_time)
-            run_valid(test_batch, model, sess)
-            thismodel_dir = os.path.join(save_dir, col)
-            if not os.path.exists(thismodel_dir):
-              os.makedirs(thismodel_dir)
-            model.saver.save(sess, os.path.join(thismodel_dir, ("%s-model.ckpt" % col)), 
-                global_step=global_step)  
+            run_valid(valid_batch, model, sess)
+            # model.saver.save(sess, os.path.join(save_dir, "model.ckpt"), global_step=global_step)  
             step_start_time = time.time()
 
-          if global_step > 30000:
+          if global_step > 8000:
             break
-
-        if global_step > 30000:
-          break
-
+        
         epoch_time = time.time() - epoch_start_time
         print "%.2f seconds in this epoch" % (epoch_time)
         print "train loss %f" % (loss / total_comments)
 
-def run_valid(test_data, model, sess):
+      test_prob.append(run_test(args, model, sess))
+
+  preds = np.zeros((test_prob[0].shape[0], len(target_cols)))
+  for prob in test_prob:
+    preds += prob
+    print prob[0]
+  preds /= len(test_prob)
+  print len(test_prob)
+  write_results(preds)
+
+def run_valid(valid_data, model, sess):
   total_logits = []
   total_labels = []
-  for batch in test_data:
+  for batch in valid_data:
     comments, comments_length, labels = batch
     loss_t, logits_t, batch_size = model.test(sess, comments, comments_length, labels, 1.0)
     total_logits += logits_t.tolist()
     total_labels += labels
   auc = roc_auc_score(np.array(total_labels), np.array(total_logits))
-  print "auc %f in valid comments" % auc  
+  auc_tf = tf.metrics.auc(labels=np.array(total_labels), predictions=np.array(total_logits))
+  sess.run(tf.local_variables_initializer())
+  auc_tf = sess.run(auc_tf)
+  print "auc %f in %d valid comments %f" % (auc, np.array(total_logits).shape[0], auc_tf[1])  
+
+def run_test(args, model, sess):
+  save_dir = os.path.join(args.save_dir, args.model_type)
+  test = pd.read_csv(args.test_dir)
+  x_test = test["comment_text"]
+  target_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+
+  x = []
+  for line in x_test:
+    if len(line) > 0:
+      x.append(utils.review_to_wordlist(line.strip()))
+  print "loaded %d comments from test dataset" % len(x)
+
+  index2word, word2index = utils.load_vocab(args.vocab_dir)
+  x_vector = utils.vectorize(x, word2index, verbose=False)
+
+  if args.model_type in ["cnn", "fnn"]:
+    test_batch = utils.get_test_batches(x_vector, args.max_size_cnn, args.max_len)
+  elif args.model_type in ["rnn"]:
+    test_batch = utils.get_test_batches(x_vector, args.max_size_rnn, type="rnn")
+
+  total_logits = []
+  for batch in test_batch:
+    comments, comments_length = batch
+    logits = model.get_logits(sess, comments, comments_length, 1.).tolist()
+    total_logits += logits
+
+  return np.array(total_logits)
 
 def test(args):
   save_dir = os.path.join(args.save_dir, args.model_type)
@@ -176,12 +209,12 @@ def test(args):
 
 def write_results(logits):
   cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-  submission = pd.read_csv('data/sample_submission.csv')    
+  submission = pd.read_csv('../data/sample_submission.csv')    
   submid = pd.DataFrame({'id': submission["id"]})
   submission = pd.concat([submid, pd.DataFrame(logits, columns=cols)], axis=1)
   submission.to_csv('submission.csv', index=False)
+  print submission.shape
 
 if __name__ == '__main__':
   args = config.get_args()
   main(args)
-  test(args)
